8e2fa45dfef829641f3096217158d9e15fe0c239
PeterCxy <xqsx43cxy@126.com>
2014-01-19 14:38:13 +0800

    wifi: fix lib including

--- wifi/Android.mk
+++ wifi/Android.mk
@@ -67,7 +67,8 @@ endif
 
 ifeq ($(BOARD_USE_XIAOMI_MIONE_HARDWARE),true)
     LOCAL_CFLAGS += -DXIAOMI_MIONE_WIFI
-    LOCAL_SHARED_LIBRARIES +=  libreadmac
+    LOCAL_SHARED_LIBRARIES += libnv liboncrpc
+    LOCAL_STATIC_LIBRARIES +=  libreadmac
 endif
 
 ifeq ($(BOARD_USE_XIAOMI_MITWO_HARDWARE),true)
